8525c58d58d9d263369586c111d0c56edd75c411
Mauritonio118 <mauro.ojeda.f@gmail.com>
2025-02-18 03:37:25 -0300

    Introduce IntegratedMinter and refactor game UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import styles from "./page.module.css";
 import { SignIn } from "../components/SignIn";
-import { Minter, Clicker, Burner, PointsDisplay, ClickCounter } from "../components/GameUI";
+import { Minter, Clicker, Burner, PointsDisplay, ClickCounter, IntegratedMinter } from "../components/GameUI";
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <SignIn />
       <PointsDisplay />
-      <ClickCounter />
       <Clicker />
-      <Minter />
+      <IntegratedMinter />
       <Burner />
     </main>
   );
